47f6f74a5f6570b8c7db819e34c2b9eeefb72ec8
root <root@ag-okd-bastion.localdomain>
2020-10-11 17:12:40 +0700

    adjust timeout

--- wait_oauth.sh
+++ wait_oauth.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
   
 oc wait co/authentication --for condition=Progressing \
-    --timeout 90s
+    --timeout 120s
 oc rollout status -n openshift-authentication deployment/oauth-openshift \
-    --timeout 90s
+    --timeout 120s
 while [ -n "$(oc get pod -n openshift-authentication -o \
 jsonpath='{.items[?(@.metadata.deletionTimestamp != "")].metadata.name}')" ]
 do
